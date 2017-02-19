557fd604e7c9a079d136c76089446d9c714438ec
Yann Herklotz <ymherklotz@gmail.com>
2017-02-19 23:17:47 +0000

    Adding initial files

---
+++ led.v
@@ -0,0 +1,25 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: Yann Herklotz
+// 
+// Create Date:    23:10:42 02/19/2017 
+// Design Name: 
+// Module Name:    led 
+// Project Name: 
+// Target Devices: Papilio Pro
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module led(
+    );
+
+
+endmodule
